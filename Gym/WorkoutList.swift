1e2600615b77dea2981729739c87c94073a975f6
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-02-27 13:44:15 -0500

    Added a tableview model

---
+++ Gym/WorkoutList.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class WorkoutList: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "Gym workout"
+        return cell
+    }
+}
